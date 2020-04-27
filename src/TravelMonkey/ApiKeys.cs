c7f9d2a17d67850b9ca4dddc1f57339e5b8992a7
Dhia Yagoub <dhia.yagoub@metsys.fr>
2020-04-27 11:33:05 +0200

    Add keys, first commit

--- src/TravelMonkey/ApiKeys.cs
+++ src/TravelMonkey/ApiKeys.cs
@@ -3,12 +3,12 @@
 	public static class ApiKeys
 	{
         #warning You need to set up your API keys.
-		public static string ComputerVisionApiKey = "";
-		public static string TranslationsApiKey = "";
-		public static string BingImageSearch = "";
+		public static string ComputerVisionApiKey = "6b31f8ba71ea46f8867375403ebfcb49";
+		public static string TranslationsApiKey = "f47932a17b22400cacb3f4ac32516c29";
+		public static string BingImageSearch = "5d10554c045245e59fe322e64f88b870";
 
 		// Change this to the Azure Region you are using
-		public static string ComputerVisionEndpoint = "https://westeurope.api.cognitive.microsoft.com/";
-		public static string TranslationsEndpoint = "https://api.cognitive.microsofttranslator.com/";
+		public static string ComputerVisionEndpoint = "https://francecentral.api.cognitive.microsoft.com/";
+		public static string TranslationsEndpoint = "https://api.cognitive.microsoft.com/sts/v1.0/issuetoken";
 	}
 }
